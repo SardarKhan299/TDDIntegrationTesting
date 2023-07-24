625ff907476ef08cb7b23a9035f515fcd4707fbb
SardarkhanStella <skhan@stellatechnology.com>
2023-07-24 16:04:10 +0500

    Make a failing test for view Model to test interaction with dao.

--- app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
@@ -1,13 +1,19 @@
 package com.raywenderlich.android.wishlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.raywenderlich.android.wishlist.persistance.RepositoryImpl
 import com.raywenderlich.android.wishlist.persistance.WishlistDao
 import com.raywenderlich.android.wishlist.persistance.WishlistDaoImpl
 import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
 
+@RunWith(AndroidJUnit4::class)
 class DetailViewModelTest{
 
     @get:Rule
@@ -16,4 +22,12 @@ class DetailViewModelTest{
     private val wishListDao: WishlistDao = Mockito.spy(WishlistDaoImpl())
 
     private val detailViewModel = DetailViewModel(RepositoryImpl(wishListDao))
+
+    @Test
+    fun saveNewItemCallsDatabase(){
+        detailViewModel.saveNewItem(Wishlist("Victoria",
+                listOf("RW Android Apprentice Book", "Android phone"), 1),
+        "Smart watch")
+        verify(wishListDao).save(any())
+    }
 }
